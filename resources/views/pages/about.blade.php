6c3e351e9c25d1368760814290c076194d66e2ac
deancomeautela <deancomeautela@gmail.com>
2018-08-17 11:31:55 -0400

    initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'name not available in .env')}}</title>
+
+    
+    </head>
+    <body>
+       <h1>This is the main Secret Santa page</h1>
+    </body>
+</html>
